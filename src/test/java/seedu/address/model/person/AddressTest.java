dada578963ed6a13868f6940d9902c1f1a4bc96f
Jolene <jolenechong7@gmail.com>
2026-02-18 18:11:40 +0800

    feat: add remark command

--- src/test/java/seedu/address/model/person/AddressTest.java
+++ src/test/java/seedu/address/model/person/AddressTest.java
@@ -53,4 +53,29 @@ public class AddressTest {
         // different values -> returns false
         assertFalse(address.equals(new Address("Other Valid Address")));
     }
+
+    public static class RemarkTest {
+
+        @Test
+        public void equals() {
+            Remark remark = new Remark("Hello");
+
+            // same object -> returns true
+            assertTrue(remark.equals(remark));
+
+            // same values -> returns true
+            Remark remarkCopy = new Remark(remark.value);
+            assertTrue(remark.equals(remarkCopy));
+
+            // different types -> returns false
+            assertFalse(remark.equals(1));
+
+            // null -> returns false
+            assertFalse(remark.equals(null));
+
+            // different remark -> returns false
+            Remark differentRemark = new Remark("Bye");
+            assertFalse(remark.equals(differentRemark));
+        }
+    }
 }
