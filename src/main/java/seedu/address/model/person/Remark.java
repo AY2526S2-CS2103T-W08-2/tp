602924873ff143db427fe3e26ae1edeefdc76a0f
Le Hua <l3hu4l1@gmail.com>
2026-02-20 01:00:42 +0800

    Logic: Implement RemarkCommand execute() logic
    Replace the error message with the actual logic to modify the remarks
    of a person.
    
    Update RemarkCommandTest to test that execute() logic works.

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,9 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Remark should not be blank
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
