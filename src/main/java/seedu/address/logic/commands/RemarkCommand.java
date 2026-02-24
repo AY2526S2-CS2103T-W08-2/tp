14d78b18c3be51009ef8b09222326d75125c65d9
Ming Xuan <66899567+icyyolo@users.noreply.github.com>
2026-02-24 08:12:12 +0800

    Add test to check for remark command throws valid exception

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -9,8 +10,10 @@ public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
 
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
+
     @Override
-    public CommandResult execute(Model mode) {
-        return new CommandResult("Hello from remark");
+    public CommandResult execute(Model mode) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
